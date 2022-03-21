57eb95fbf8f96ad13001b129cecd37d3baa21411
이용민 <minidragon.lee@gmail.com>
2022-03-21 23:33:08 +0900

    COMMIT : Basic Func

--- src/pages/auth/FindId.tsx
+++ src/pages/auth/FindId.tsx
@@ -5,11 +5,16 @@ const FindId = () => {
 		<div className='find-wrap'>
 			<form action="">
 				<input type="email" placeholder='Email' required/>
+				<button>전송하기</button>
+			</form>
+			<form action="">
+				<input type="number" placeholder='Auth Code' required/>
 				<button>인증하기</button>
 			</form>
-			<div className="finded-id">
-				<p>회원님의 ID는 <span>abcd1234</span> 입니다.</p>
+			<div className="finded">
+				<p>회원님의 PW은 <span>&quot;abcd1234&quot;</span> 입니다.</p>
 			</div>
+			<button type='button'>로그인하러가기</button>
 		</div>
 	);
 }
--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const ResetPassword = () => {
   return (
-		<div>
-			
+		<div className='find-wrap'>
+			<form action="">
+				<input type="email" placeholder='Email' required/>
+				<button>인증하기</button>
+			</form>
+			<div className="finded">
+				<p>회원님의 ID는 <span>&quot;abcd1234&quot;</span> 입니다.</p>
+			</div>
+			<button type='button'>로그인하러가기</button>
 		</div>
 	);
 }
